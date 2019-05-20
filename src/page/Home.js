3f2dc2f9155c9bcb7512473a9e9bc3d0ca2e336f
Wahyu Pambudi <wahyupambudi823@gmail.com>
2019-05-20 21:56:28 +0700

    Crud-Reactjs

--- src/page/Home.js
+++ src/page/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
   <div>
       <br></br>
     <h1 id="edit1">Welcome On Home Page</h1>
-    <p id="edit2"><h2>Please Log In To View Data</h2></p>
+    <p id="edit2"><h4>Please Log In To View Data</h4></p>
   </div>
 );
 
